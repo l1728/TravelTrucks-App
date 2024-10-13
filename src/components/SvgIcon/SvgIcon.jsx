ea93ef06961a6808e1df77a0177e0838f05babb8
Tetyana Gogulya <cheburashka077@ukr.net>
2024-10-13 14:18:33 +0300

    corr_path

--- src/components/SvgIcon/SvgIcon.jsx
+++ src/components/SvgIcon/SvgIcon.jsx
@@ -4,7 +4,7 @@ import css from './SvgIcon.module.css';
 const SvgIcon = ({ id, width = 32, height = 32, className = '' }) => {
   return (
     <svg className={`${css.icon} ${className}`} width={width} height={height}>
-      <use href={`/public/icons.svg#${id}`} />
+      <use href={`/public/img/icons.svg#${id}`} />
     </svg>
   );
 };
